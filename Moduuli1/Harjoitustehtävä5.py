ca814d2a899f196c5b4450fca3067c059eaf6463
zymet <zymet93@gmail.com>
2023-01-12 11:57:11 +0200

    Tehtävä3

---
+++ Moduuli1/Harjoitustehtävä5.py
@@ -0,0 +1,13 @@
+LUOTI_KERROIN = 13.3
+NAULA_KERROIN = 32 * LUOTI_KERROIN
+LEIVISKÄ_KERROIN = 20 * NAULA_KERROIN * LUOTI_KERROIN
+
+leiviskätGrammoina = float(input("Anna leiviskät: ")) * LEIVISKÄ_KERROIN
+naulatGrammoina = float(input("Anna naulat: ")) * NAULA_KERROIN
+luoditGrammoina = float(input("Anna luodit: ")) * LUOTI_KERROIN
+
+grammatyhteensä = leiviskätGrammoina + naulatGrammoina + luoditGrammoina
+kilogrammat = int(grammatyhteensä / 1000)
+grammat = int(grammatyhteensä % 1000)
+
+print(f'Massa nykymittojen mukaan:\n{kilogrammat} kilogrammaa ja {grammat} grammaa. ')
