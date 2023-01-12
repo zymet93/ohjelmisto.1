d835661d7e79a5616dcf4787dfc4d7866182b65e
zymet <zymet93@gmail.com>
2023-01-12 12:19:00 +0200

    Tehtävä4

---
+++ Moduuli1/harjoitustehtävä4.py
@@ -0,0 +1,11 @@
+"""Ohjelma tulostaa lukujen summan, tulon ja keskiarvon."""
+num1=int(input('anna luku1'))
+num2=int(input('anna luku2'))
+num3=int(input('anna luku3'))
+
+summa =num1+num2+num3
+tulo = num1num2num3
+
+print('tulos',tulo)
+print('summa',summa)
+print('keskiarvo',summa/3)
