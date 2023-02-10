b897dfb915c785c0f8e25ecb73dcbc68e860d2de
zymet93 <122457245+zymet93@users.noreply.github.com>
2023-02-10 12:37:09 +0200

    Update Tehtävä1.py

--- Moduuli3/Tehtävä1.py
+++ Moduuli3/Tehtävä1.py
@@ -2,7 +2,7 @@
 # laskea kuhan takaisin järveen ilmoittaen samalla käyttäjälle,
 # montako senttiä alimmasta sallitusta pyyntimitasta puuttuu. Kuha on alamittainen, jos sen pituus on alle 37 cm.
 
-x = int(input("Ilmoita kuhan pituus"))
+x = int(input("Ilmoita kuhan pituus sentteinä: "))
 if x < 37:
     print("Laske kuha takaisin järveen")
     print(f"{37-x} Senttiä alimmasta sallitusta pyyntimitasta")
